32ad80c13873c44c8f66b84b5b2e2f3d3fc697db
FahimAhamed0602 <personalresource2022@gmail.com>
2025-06-29 21:34:48 +0600

    done

--- hanker rank_1.py
+++ hanker rank_1.py
@@ -1,12 +1,16 @@
 def print_rangoli(size):
-    # your code goes here
-    l1=list(map(chr,range(97,122)))
-    x=l1[n-1::-1]+l1[1:n]
-    m=len('-'.join(x))
-    for i in range(1,n):
-        print('-'.join(l1[n-1:n-i:-1]+l1[n-i:n]).center(m,'-'))
-    for i in range(n,0,-1):
-        print('-'.join(l1[n-1:n-i:-1]+l1[n-i:n]).center(m,'-'))
+    # Create a list of lowercase letters a-z
+    l1 = list(map(chr, range(97, 123)))
+    # Create the widest row (e.g., for n=3: c-b-a-b-c)
+    x = l1[size-1::-1] + l1[1:size]
+    # Calculate the width of the rangoli
+    m = len('-'.join(x))
+    # Upper half of the rangoli
+    for i in range(1, size):
+        print('-'.join(l1[size-1:size-i:-1] + l1[size-i:size]).center(m, '-'))
+    # Full rangoli including the middle row and lower half
+    for i in range(size, 0, -1):
+        print('-'.join(l1[size-1:size-i:-1] + l1[size-i:size]).center(m, '-'))
 
 if __name__ == '__main__':
     n = int(input())
